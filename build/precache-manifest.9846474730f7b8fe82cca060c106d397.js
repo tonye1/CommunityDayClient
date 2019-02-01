3cad980fdd1dd89b2da678a67bbfdfadff82d33a
Tony Efremenko <tonye1@us.ibm.com>
2019-02-01 13:45:44 -0500

    initial commit

---
+++ build/precache-manifest.9846474730f7b8fe82cca060c106d397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afd42ac4860b66e318c4",
+    "url": "/static/css/main.670ac0ff.chunk.css"
+  },
+  {
+    "revision": "afd42ac4860b66e318c4",
+    "url": "/static/js/main.afd42ac4.chunk.js"
+  },
+  {
+    "revision": "223686a1aa4818ccab6f",
+    "url": "/static/js/1.223686a1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ffff0da7ab6e4a4434a5967a3781bcb1",
+    "url": "/static/media/market.ffff0da7.png"
+  },
+  {
+    "revision": "df05769e0c1d9b41934205a72cb354ec",
+    "url": "/index.html"
+  }
+];
